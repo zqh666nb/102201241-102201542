7e4af0afa5a6dd4b3b0c8d2b03b500389d237b2e
dky <119654248+dky1126@users.noreply.github.com>
2024-10-09 11:18:57 +0800

    Add files via upload

--- miniprogram/pages/create/create.ts
+++ miniprogram/pages/create/create.ts
@@ -2,18 +2,34 @@ const app = getApp();
 
 Page({
   data: {
+    nickname:'',
     title: '',
     description: ''
   },
 
+  // 在页面加载时初始化输入框
   onLoad() {
-    // 确保在页面加载时清空输入框
+    this.resetForm(); // 这里调用重置方法
+  },
+
+  // 当页面显示时重置输入框
+  onShow() {
+    this.resetForm(); // 每次显示页面时重置
+  },
+
+  // 重置输入框的值
+  resetForm() {
     this.setData({
+      nickname: '',
       title: '',
       description: ''
     });
   },
-
+  onnameChange(event:any) {
+    this.setData({
+      nickname: event.detail.value
+    });
+  },
   onTitleChange(event:any) {
     this.setData({
       title: event.detail.value
@@ -27,23 +43,36 @@ Page({
   },
 
   submitProject() {
-    const { title, description } = this.data;
+    const {nickname, title, description } = this.data;
 
-    if (!title || !description) {
+    if (!nickname) {
       wx.showToast({
-        title: '请填写项目名称和描述',
+        title: '请填写创建者昵称',
         icon: 'none'
       });
       return;
     }
-
+    if (!title) {
+      wx.showToast({
+        title: '请填写项目标题',
+        icon: 'none'
+      });
+      return;
+    }
+    if ( !description) {
+      wx.showToast({
+        title: '请填写项目描述',
+        icon: 'none'
+      });
+      return;
+    }
     // 发布项目到数据库
     // 获取当前用户的昵称
 wx.getUserProfile({
   desc: '用于获取创建者的昵称', // 授权说明
-  success: (userProfile) => {
-    const nickname = userProfile.userInfo.nickName; // 获取用户昵称
-    const { title, description } = this.data; 
+  success: () => {
+    
+    const { nickname, title, description } = this.data; 
 
     // 将项目数据，包括昵称，添加到数据库
     wx.cloud.database().collection('projects').add({
